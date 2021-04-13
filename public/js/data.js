de47292d537532160a8350e921fbc0a279c29c80
Denis Dorogov <denisdorogov@yandex.ru>
2021-04-13 22:05:57 +0500

    added const params

--- public/js/data.js
+++ public/js/data.js
@@ -31,5 +31,22 @@ const STORAGE_DEFAULT = {
         averageResult: 0,
         lastResult: 0,
     },
-    resetDate: Date.now(),
-}
+    mixed: {
+        lastDate: Date.now(),
+        lastTime: undefined,
+        countAttempts: 0,
+        averageResult: 0,
+        lastResult: 0,
+    },
+    resetDate: Date.now()
+};
+
+const PARAMS = {
+    difficulty: {
+        summ: [[1, 10], [1, 20], [1, 100], [19, 100]],
+        diff: [[10, 0], [20, 0], [100, 0], [100,20]],
+        mult: [[1,4], [3, 6], [5, 9], [2, 10]],
+        div: [[1,4], [3, 6], [5, 9], [2, 10]],
+        mix: []
+    },
+};
