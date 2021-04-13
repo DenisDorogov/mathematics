b39c4ed79f06d305e4693eb1dd82c2e5d2176546
Denis Dorogov <denisdorogov@yandex.ru>
2021-04-13 22:07:47 +0500

    edit class. added methods, fill constuctor

--- public/js/Operation.js
+++ public/js/Operation.js
@@ -1,3 +1,56 @@
 class Operation {
-    
+    arrayExamples = [];
+    countExamples;
+    operandMax;
+    operandMin;
+
+    constructor(type, difficultLevel = 1, courses) {
+        switch (type) {
+            case 'summ':
+                this.getArraySumm(difficultLevel);
+                break;
+            case 'diff':
+                this.getArrayDiff(difficultLevel);
+                break;
+            case 'mult':
+                this.getArrayMult(difficultLevel);
+                break;
+            case 'div':
+                this.getArrayDiv(difficultLevel);
+                break;
+            case 'mix':
+                this.getArrayMix(difficultLevel,courses);
+                break;
+            default:
+                break;
+        }
+        this.countExamples = difficultLevel * 10;
+        this.operandMin = PARAMS.difficulty[type][difficultLevel][0];
+        this.operandMax = PARAMS.difficulty[type][difficultLevel][1];
+    }
+
+    getArraySumm(level) {
+        for ( let i = this.operandMax; i > this.operandMin; i--) {
+            for (let j = 1; j < i; j++) {
+                this.arrayExamples.push([i - j + ' + ' + j, i ]);
+            }
+        }
+
+        console.log(this.operandMax);
+        console.log(this.operandMin);
+        console.log(this.arrayExamples);
+
+    }
+
+    getArrayDiff(level) {
+
+    }
+
+    getArrayMult(level) {
+
+    }
+
+    getArrayDiv(level) {
+
+    }
 }
